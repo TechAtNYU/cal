fc1e1deb243b385315dde79511522bc2829dcfa6
Omayeli Arenyeka <aomayeli@gmail.com>
2016-02-16 08:31:55 -0500

    removing logs and backbone dependency

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,6 @@ require.config({
   paths: {
     'jquery': 'vendor/jquery/dist/jquery',
     'underscore': 'vendor/underscore-amd/underscore',
-    'backbone': 'vendor/backbone-amd/backbone',
     'moment': 'vendor/moment/moment',
     'clndr': 'vendor/clndr/clndr.min',
   }
