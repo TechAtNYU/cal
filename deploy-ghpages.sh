5e3fa592fb52e52ca394801f7d9f08f9fe147ca0
Ashley Teoh <ashleytqy@gmail.com>
2018-05-19 14:51:12 -0400

    dont error if no changes

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -7,20 +7,7 @@ set -e
 pwd
 remote=$(git config remote.origin.url)
 
-# now lets setup a new repo so we can update the gh-pages branch
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
-
-git add app && git commit -m "Initial app subtree commit"
-git subtree push --prefix app origin gh-pages
-
-git checkout gh-pages
-git merge master --no-edit
-npm install && npm run-script build
-
-
-git add .
-git commit -m "building into gh-pages `date`"
-git push
+git add app && git commit -m "Initial app subtree commit" > /dev/null 2>&1
+git subtree push --prefix app origin gh-pages > /dev/null 2>&1
 
 echo "Finished Deployment!"
