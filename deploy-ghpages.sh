b693dea551045f16199e3593c7678bbc58cadf0e
Ashley Teoh <ashleytqy@gmail.com>
2018-05-19 14:57:36 -0400

    syntax error

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -6,12 +6,14 @@ set -e
 # show where we are on the machine
 pwd
 
-if git diff-index --quiet HEAD --; then
+if git diff-index --quiet HEAD --
+then
     # no changes
     echo "No changes."
 else
-  git add app && git commit -m "Initial app subtree commit"
-  git subtree push --prefix app origin gh-pages
+  echo "changes"
+  # git add app && git commit -m "Initial app subtree commit"
+  # git subtree push --prefix app origin gh-pages
 fi
 
 echo "Finished Deployment!"
